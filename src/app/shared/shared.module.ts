ac844f0c1f6f9d542215fb6396d22f6cc5a16c68
med-amine01 <m.chebbi.tech@gmail.com>
2024-12-03 11:17:38 +0100

    add property type

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { FormsModule } from '@angular/forms';
+import { PropertyTypePipe } from './pipes/property-type.pipe';
 
 @NgModule({
-  declarations: [PaginationComponent],
+  declarations: [PaginationComponent, PropertyTypePipe],
   imports: [CommonModule, FormsModule],
-  exports: [PaginationComponent],
+  exports: [PaginationComponent, PropertyTypePipe],
 })
 export class SharedModule {}
